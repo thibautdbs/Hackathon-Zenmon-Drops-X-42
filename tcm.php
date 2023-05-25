d9abb64bc4a70a2f3c3471cebee414d40e6eaebe
thibaut dubois <thibautdbs@outlook.com>
2023-05-26 01:19:43 +0200

    updated styles and add arguments to shortcode
    - added styles to the form input field
    - added form reset on validation from user
    - cleaned list item generation functions
    - added arguments to the short code:
      - width: (default: inherit) administrator can specify
        the width of the chatbot component (same as css format).
      - min-height: (default: inherit) same behavior.
      - max-height: (default: inherit) if set, a custom scroll bar
        show's up when necessary.

--- tcm.php
+++ tcm.php
@@ -4,18 +4,28 @@
 * Plugin URI: https://github.com/thibautdbs/Hackathon-ZenmonDrops-Enonce2-42XZD
 * Description: A neuroscience chatbot.
 * Version: 0.1
-* Author: tdubois
+* Author: cmassera, ffeauga, tdubois
 * Author URI: https://github.com/thibautdbs
 **/
 
+/**
+ * Show_chatbot
+ *
+ * @param  array  $atts     Shortcode attributes. Default empty.
+ * @return string Shortcode output.
+ */
 function tcm_show($atts = array())
 {
-    // $wporg_atts = shortcode_atts(
-	// 	array(
-	// 		'width' => 'inherit',
-    //         'height' => 'inherit',
-	// 	), $atts, $tag
-	// );
+    // Normalize attribute keys, lowercase
+    $atts = array_change_key_case((array) $atts, CASE_LOWER);
+
+    $tcm_atts = shortcode_atts(
+		array(
+			'width' => 'inherit',
+            'max-height' => 'inherit',
+            'min-height' => '0px',
+		), $atts
+	);
 
     ob_start(); ?>
 <!-- HTML code that will replace [tcm] shortcode ------------------------------>
@@ -23,85 +33,150 @@ function tcm_show($atts = array())
 
     <!-- style of the chatbot -->
     <style>
-        .tcm-main
+        #tcm-chatbot
         {
-            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+
+            width: <?php echo $tcm_atts['width'] ?>;
+            max-height: <?php echo $tcm_atts['max-height'] ?>;
+            min-height: <?php echo $tcm_atts['min-height'] ?>;
+
+            padding: 5px;
             border-radius: 10px;
             box-shadow: 2px 2px 8px -2px #464434;
         }
 
-        .tcm-list
+        #tcm-list
         {
+            margin: 0;
+            padding: 0;
+
+            overflow-y: auto;
+            overflow-x: hidden;
+            word-wrap: break-word;
+
             display: flex;
-            padding:5px;
-            flex-direction: column;
             gap: 5px;
-            
-            overflow: hidden;
+            flex-direction: column;
         }
 
-        .tcm-list > li
+        #tcm-list /* firefox custom scroll bar */
         {
-            border-radius: 5px;
-            list-style: none;
+            scrollbar-width: thin;
+            scrollbar-color: #888 #f1f1f1;
         }
-        
-        .tcm-list > li span
+
+        #tcm-list::-webkit-scrollbar /* webkit custom scroll bar */
+        {
+            width: 5px;
+        }
+
+        #tcm-list::-webkit-scrollbar-track /* webkit custom scroll bar */
+        {
+            background: #f1f1f1;
+        }
+
+        #tcm-list::-webkit-scrollbar-thumb /* webkit custom scroll bar */
+        {
+            background-color: #888;
+            border-radius: 20px;
+        }
+
+        #tcm-list li:last-child /* add margin only if there's a msg */
+        {
+            margin-bottom: 10px;
+        }
+
+        .tcm-botmsg
+        {
+            list-style: none;
+            padding: 5px;
+            border-radius: 5px;
+            background-color: #C3F8FF;
+        }
+        .tcm-botmsg span
         {
             margin: 0;
             margin-right: 5px;
             padding: 0px 10px;
             border-radius: 15px;
-        }
-
-        .tcm-list > li p
-        {
-            margin: 0;
-            padding: 5px;
-        }
-
-        .tcm-user-msg
-        {
-            background-color: #C3F8FF;
-        }
-
-        .tcm-user-msg span
-        {
             background-color: #ABD9FF;
         }
 
-        .tcm-bot-msg
+        .tcm-usermsg
         {
+            list-style: none;
+            padding: 5px;
+            border-radius: 5px;
             background-color: #FFF6BF;
         }
         
-        .tcm-bot-msg span
+        .tcm-usermsg span
         {
+            margin: 0;
+            margin-right: 5px;
+            padding: 0px 10px;
+            border-radius: 15px;
             background-color: #ffd966;
+        }
+
+        #tcm-form
+        {
+            display: flex;
+            flex-direction: row;
+
+            border: 1px solid #ccc;
+            border-radius: 20px;
+
+            padding: 10px;
+        }
+
+        #tcm-form:focus-within
+        {
+            border: 1px solid #888;
+        }
+
+        #tcm-input
+        {
+            flex-grow: 2;
+            border: none;
+            outline: none;
+        }
+
+        #tcm-input:focus
+        {
+            outline: none;
+        }
+
+        #tcm-button
+        {
+            border-radius: 20px;
         }
     </style>
 
     <!-- the chatbot container -->
-    <div id="chatbot" class="tcm-main">
-        <ul id="chatbot-list" class="tcm-list">
+    <div id="tcm-chatbot">
+        <ul id="tcm-list">
         </ul>
         <form id="tcm-form" action="/api/process/form" method="post">
             <input type="text" name="tcm-input" id="tcm-input" required>
-            <button type="submit">SubmitAction</button>
+            <button type="submit" id="tcm-button">Send</button>
         </form>
     </div>
 
     <!-- javascript code behind the chat bot -->
     <script>
-        const chatbot = document.getElementById("chatbot");
-        const chatbotList = document.getElementById("chatbot-list");
-        
+        const chatbot = document.getElementById("tcm-chatbot");
+        const chatbotList = document.getElementById("tcm-list");
+
         function appendBotMsg(msg)
         {            
             node = document.createElement("div");
             node.innerHTML = 
-                `<li class="tcm-bot-msg">
-                    <p><span>Bot</span>${msg}</p>
+                `<li class="tcm-botmsg">
+                    <span>Bot</span>
+                    ${msg}
                 </li>`;
             chatbotList.appendChild(node.firstChild);
         }
@@ -110,21 +185,20 @@ function tcm_show($atts = array())
         {
             node = document.createElement("div");
             node.innerHTML = 
-                `<li class="tcm-user-msg">
-                    <p><span>User</span>${msg}</p>
+                `<li class="tcm-usermsg">
+                    <span>User</span>
+                    ${msg}
                 </li>`;
             chatbotList.appendChild(node.firstChild);
         }
 
-        appendBotMsg("msg2")
-        appendUserMsg("msg3")
-        appendBotMsg("msg4")
-
         document.forms['tcm-form'].addEventListener('submit', (event) => {
             event.preventDefault();
+
             // TODO do something here to show user that form is being submitted
             data = new FormData(event.target);
             appendUserMsg(data.get("tcm-input"));
+            event.target.reset();
 
             fetch(event.target.action, {
                 method: 'POST',
@@ -140,7 +214,6 @@ function tcm_show($atts = array())
                 // TODO handle error
             });
         });
-
     </script>
     
 <!-- end of HTML code --------------------------------------------------------->
@@ -163,7 +236,10 @@ function tcm_shortcode($atts = array())
     return tcm_show($atts);
 }
 
-// register the [tcm] short code to be used in wordpress pages
-add_shortcode('tcm', 'tcm_shortcode');
+function tcm_shortcodes_init()
+{
+    add_shortcode('tcm', 'tcm_shortcode');
+}
 
+add_action('init', 'tcm_shortcodes_init');
 ?>
