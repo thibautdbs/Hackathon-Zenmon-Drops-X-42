84aa48b7e5d8aae58b68b30817e381eed10b30c6
DoubleSharky67 <frofeaugas@gmail.com>
2023-05-25 18:08:15 +0200

    First request working version : Request model invalid however

--- tcm.php
+++ tcm.php
@@ -8,6 +8,45 @@
 * Author URI: https://github.com/thibautdbs
 **/
 
+function genererTexteChatGPT($prompt, $apiKey)
+{
+    $apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+    $data = array(
+        'prompt' => $prompt,
+        'max_tokens' => 50,
+        'temperature' => 0.8
+    );
+
+    $headers = array(
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $apiKey
+    );
+
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        echo 'RATE';
+        echo 'Erreur lors de lappel à lAPI : ' . curl_error($ch);
+        return null;
+    } else {
+        echo 'ca marche';
+        var_dump($response);
+        $responseData = json_decode($response, true);
+        var_dump($responseData);
+        $texteGenere = $responseData['choices'][0]['message']['content'];
+        echo $texteGenere;
+        return $texteGenere;
+    }
+    curl_close($ch);
+}
+
 function tcm_show()
 {
     ob_start(); ?>
@@ -77,7 +116,11 @@ function tcm_show()
 
 function tcm_shortcode() 
 {
-    return tcm_show();
+    $stp = 'test : ';
+    echo $stp;
+    $stp = genererTexteChatGPT('reponds par oui ou par non : Paris est elle la capitale de la France ?', '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');
+    echo $stp;
+    // return tcm_show();
 }
 
 // register the [tcm] short code to be used in wordpress pages
