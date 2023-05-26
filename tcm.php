98da3ea7e36d2c0ce98db252d63b70fc00e88754
thibautdbs <tdubois@student.42angouleme.fr>
2023-05-26 09:19:01 +0200

    added fixed position option and custom font
    - [tcm fixed=true] will attach the chatbot to the bottom of the page
    - add a custom font and svg icon to the button
    - fixed scroll position when max-height is set

--- tcm.php
+++ tcm.php
@@ -24,6 +24,7 @@ function tcm_show($atts = array())
 			'width' => 'inherit',
             'max-height' => 'inherit',
             'min-height' => '0px',
+            'fixed' => false,
 		), $atts
 	);
 
@@ -33,16 +34,30 @@ function tcm_show($atts = array())
 
     <!-- style of the chatbot -->
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display&display=swap');
+
         #tcm-chatbot
         {
+            font-family: 'Red Hat Display', sans-serif;
+            <?php
+                if ($tcm_atts['fixed'])
+                {
+                    echo 'position: fixed;';
+                    echo 'bottom: 0px;';
+                    echo 'right: 5%;';
+                } 
+            ?>
+            margin-bottom: 0;
             display: flex;
             flex-direction: column;
+            justify-content: space-between;
 
             width: <?php echo $tcm_atts['width'] ?>;
             max-height: <?php echo $tcm_atts['max-height'] ?>;
             min-height: <?php echo $tcm_atts['min-height'] ?>;
 
             padding: 5px;
+            background-color: white;
             border-radius: 10px;
             box-shadow: 2px 2px 8px -2px #464434;
         }
@@ -151,7 +166,18 @@ function tcm_show($atts = array())
 
         #tcm-button
         {
+            font-family: 'Red Hat Display', sans-serif;
             border-radius: 20px;
+            padding: 0 2px;
+        }
+
+        #tcm-button svg
+        {
+            position: relative;
+            top: 1.5px;
+            left: 0.15px;
+            width: 20px;
+            height: 20px;
         }
     </style>
 
@@ -161,7 +187,25 @@ function tcm_show($atts = array())
         </ul>
         <form id="tcm-form" action="/api/process/form" method="post">
             <input type="text" name="tcm-input" id="tcm-input" required>
-            <button type="submit" id="tcm-button">Send</button>
+            <button type="submit" id="tcm-button">
+                <svg 
+                        version="1.1"
+                        id="Layer_1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" 
+                        viewBox="0 0 32 32"
+                        xml:space="preserve">
+                    <ellipse style="fill:none;stroke:#000000;stroke-width:2;stroke-miterlimit:10;" cx="12.5" cy="9.5" rx="2.5" ry="3.5"/>
+                    <ellipse style="fill:none;stroke:#000000;stroke-width:2;stroke-miterlimit:10;" cx="19.5" cy="9.5" rx="2.5" ry="3.5"/>
+                    <ellipse style="fill:none;stroke:#000000;stroke-width:2;stroke-miterlimit:10;" cx="7.5" cy="16.5" rx="2.5" ry="3.5"/>
+                    <ellipse style="fill:none;stroke:#000000;stroke-width:2;stroke-miterlimit:10;" cx="24.5" cy="16.5" rx="2.5" ry="3.5"/>
+                    <path 
+                            style="fill:none;stroke:#000000;stroke-width:2;stroke-miterlimit:10;"
+                            d="M19,20c-0.966-0.966-1-3-3-3s-2,2-3,3
+	s-4,1.069-4,3.5c0,1.381,1.119,2.5,2.5,2.5c1.157,0,3.684-1,4.5-1s3.343,1,4.5,1c1.381,0,2.5-1.119,2.5-2.5
+	C23,21.207,19.966,20.966,19,20z"/>
+                </svg>
+            </button>
         </form>
     </div>
 
@@ -199,6 +243,7 @@ function tcm_show($atts = array())
             data = new FormData(event.target);
             appendUserMsg(data.get("tcm-input"));
             event.target.reset();
+            chatbotList.scrollTo(0, chatbotList.scrollHeight);
 
             fetch(event.target.action, {
                 method: 'POST',
